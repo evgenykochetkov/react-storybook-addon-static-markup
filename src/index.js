3354be4a5a7e904db8b0af774a16a8aed2166d11
Rob Tarr <rob@robtarr.net>
2017-06-09 21:54:28 -0400

    Add pretty to format the HTML output

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import addons from '@kadira/storybook-addons';
+import pretty from 'pretty';
 
 import ReactDOMServer from 'react-dom/server'
 
@@ -7,7 +8,7 @@ export class ShowStaticMarkup extends React.Component {
   render() {
     const { children } = this.props;
 
-    const markup = ReactDOMServer.renderToStaticMarkup(children)
+    const markup = pretty(ReactDOMServer.renderToStaticMarkup(children));
 
     const channel = addons.getChannel();
     channel.emit('evgenykochetkov/static-markup/show-markup', markup);
